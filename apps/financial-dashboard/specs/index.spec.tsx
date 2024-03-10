2060e4756220025d159f3c21bc0130cfc38c2c17
Attila Csanyi <attila.csnyi@yahoo.co.uk>
2024-03-10 18:31:37 +0100

    fix(test): mock next font loader (https://github.com/vercel/next.js/issues/59701)

--- apps/financial-dashboard/specs/index.spec.tsx
+++ apps/financial-dashboard/specs/index.spec.tsx
@@ -1,7 +1,19 @@
-import React from 'react';
 import { render } from '@testing-library/react';
 
 import Page from '../src/app/page';
+
+jest.mock('next/font/google', () => ({
+  Inter: () => ({
+    style: {
+      fontFamily: 'mocked',
+    },
+  }),
+  Lusitana: () => ({
+    style: {
+      fontFamily: 'mocked',
+    },
+  }),
+}));
 
 describe('Page', () => {
   it('should render successfully', () => {
